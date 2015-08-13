bf2df277a068a96792a85630e3d89545a4e422cc
Gilles Migliori <gilles.migliori@gmail.com>
2015-08-13 17:22:04 +0200

    First commit

---
+++ PHP Form Builder.py
@@ -0,0 +1,78 @@
+import sublime, sublime_plugin
+
+class CompletionTriggerCommand(sublime_plugin.TextCommand):
+    def run(self, edit, completionTriggerText):
+        self.view.insert(edit, self.view.sel()[0].begin(), completionTriggerText)
+        self.view.run_command("auto_complete")
+        self.view.run_command("insert_best_completion")
+
+class phpFormBuilderCommand(sublime_plugin.TextCommand):
+    def on_quick_panel_selection(self, index):
+        if index == -1:
+            return
+        else:
+            self.view.run_command('completion_trigger', {"completionTriggerText": self.completions_trigger[index]})
+    def run(self, edit):
+        self.form_element_labels = [
+            'new Form($form_ID, $layout = \'horizontal\', $attr = \'\');',
+            'setAction($url, $add_get_vars = true)',
+            'setOptions();',
+            'startFieldset($legend = \'\')',
+            'endFieldset()',
+            'addInputWrapper($html, $element_name)',
+            'groupInputs($input1, $input2, $input3 = \'\')',
+            'addInput($type, $name, $value = \'\', $label = \'\', $attr = \'\')',
+            'addTextarea($name, $value = \'\', $label = \'\', $attr = \'\')',
+            'addOption($select_name, $value, $txt, $group_name = \'\', $attr = \'\')',
+            'addSelect($select_name, $label = \'\', $attr = \'\', $displayGroupLabels = true)',
+            'addCountrySelect($select_name, $label = \'\', $attr = \'\', $user_options = array())',
+            'addRadio($group_name, $label, $value, $attr)',
+            'printRadioGroup($group_name, $label = \'\', $inline = true, $attr = \'\')',
+            'addCheckbox($group_name, $label, $name, $value, $attr)',
+            'printCheckboxGroup($group_name, $label = \'\', $inline = true, $attr = \'\')',
+            'addBtn($type, $name, $value, $text, $attr = \'\', $btnGroupName = \'\')',
+            'printBtnGroup($btnGroupName, $label = \'\')',
+            'addHtml($html, $element_name = \'\', $pos = \'after\')',
+            'addPlugin($plugin_name, $selector, $js_content = \'default\', $js_replacements = \'\')',
+            'addFileUpload($type, $name, $value = \'\', $label = \'\', $attr = \'\', $fileUpload_config = \'\')',
+            'printIncludes($type)',
+            'render($debug = false)',
+            'printJsCode()',
+            'Form::sendMail($from_email, $adress, $subject, $filter_values = \'\', $values = \'\')',
+            'Form::sendAdvancedMail($options, $values = \'\')',
+            'Form::registerValues($form_ID)',
+            'Form::mergeValues(array(\'step-form-1\', \'step-form-2\', \'step-form-3\'))',
+            'Form::clear($form_ID)'
+        ]
+        self.completions_trigger = [
+            'newForm',
+            'setAction',
+            'setOptions',
+            'startFieldset',
+            'endFieldset',
+            'addInputWrapper',
+            'groupInputs',
+            'addInput',
+            'addTextarea',
+            'addOption',
+            'addSelect',
+            'addCountrySelect',
+            'addRadio',
+            'printRadioGroup',
+            'addCheckbox',
+            'printCheckboxGroup',
+            'addBtn',
+            'printBtnGroup',
+            'addHtml',
+            'addPlugin',
+            'addFileUpload',
+            'printIncludes',
+            'render',
+            'printJsCode',
+            'FormSendMail',
+            'FormSendAdvancedMail',
+            'FormRegisterValues',
+            'FormMergeValues',
+            'FormClear'
+        ]
+        self.view.window().show_quick_panel(self.form_element_labels, self.on_quick_panel_selection)
---
+++ README.md
@@ -0,0 +1,65 @@
+PHP Form Builder
+==============
+
+PHP Form Builder is a Sublime Text 2/3 plugin for [**Php Form Builder**](http://codecanyon.net/item/php-form-builder/8790160 "Php Form Builder"),  
+php class to build forms sold on [Codecanyon](http://codecanyon.net/category/php-scripts "Codecanyon").
+
+PHP Form Builder adds auto-completions for Form Builder's PHP class methods.
+
+Features
+--------
+
+* access to PHP Form Builder completions using one unique shortcut
+* access to PHP Form Builder completions using the Tools menu
+
+Installing
+----------
+
+**Package Control** (**this is the recommended method**) :
+  First install Package Control - [see installation instructions](https://packagecontrol.io/installation)
+  Then inside Sublime Text press `ctrl + shift + p` ( `super + shift + p` on Mac OS X ) and find
+  `Package Control: Install Package` then press Enter.
+  You can search for the `PHP Form Builder` package by entering its name `PHP Form Builder`<br><br>
+
+**Without Git:** Download the latest source from [GitHub](https://github.com/migliori/sublime-phpformbuilder) and copy the PhpFormBuilder folder to your Sublime Text "Packages" directory.
+
+**With Git:** Clone the repository in your Sublime Text "Packages" directory:
+
+    git clone https://github.com/migliori/sublime-phpformbuilder.git PhpFormBuilder
+
+
+The "Packages" directory is located at:
+
+* OS X:
+
+        ST2: ~/Library/Application Support/Sublime Text 2/Packages/
+        ST3: ~/Library/Application Support/Sublime Text 3/Packages/
+
+* Linux:
+
+        ST2: ~/.config/sublime-text-2/Packages/
+        ST3: ~/.config/sublime-text-3/Packages/
+
+* Windows:
+
+        ST2: %APPDATA%/Sublime Text 2/Packages/
+        ST3: %APPDATA%/Sublime Text 3/Packages/
+
+Usage
+-----
+
+### using shortcut
+`ctrl+alt+w` or `super+alt+w`
+
+### using auto-completions
+start typing the name of the function, auto-complete list will appear.
+
+### using sublime text menu
+Tools -> PHP Form Builder -> Completions
+
+Shortcut
+--------
+
+Defaut shortcut is `ctrl+alt+w` or `super+alt+w`
+
+You can change it in Preferences -> Package Settings -> PHP Form Builder -> Key Bidings - User
